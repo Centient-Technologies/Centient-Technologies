ce539e865ad98f29a86839ddc66d500e7b4fe0a2
davidAg9 <shane_alpha9@outlook.com>
2023-10-06 09:43:43 +0000

    remove skeloton app code and added navbar

---
+++ app/about/page.tsx
@@ -0,0 +1,11 @@
+
+const About = () => {
+    return (
+        <div>
+            Enter
+        </div>
+    );
+}
+
+
+export default About
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import NavigationBar from './components/NavigationBar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,7 +19,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <NavigationBar
+          logo="/logo.jpg"
+          companyName="Centient Technologies"
+          navItems={["Home", "About", "Contact"]}
+        />
+        {children}
+      </body>
     </html>
   )
 }
