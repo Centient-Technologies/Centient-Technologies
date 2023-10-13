7fa954ad622f149eb3be5d005c546e7a5093db0f
emmanaakai <naa.kai.odametey@gmail.com>
2023-10-13 20:29:06 +0000

    industries with images added

--- app/services/components/IndustryExpertise.tsx
+++ app/services/components/IndustryExpertise.tsx
@@ -1,21 +1,38 @@
-const industries = [
-    {title: "Logistics and Transportation", image: ""},
-    {title: "Health", image: ""},
-    {title: "Finance", image: ""},
-    {title: "Communication", image: ""},
-   
-]
-export function IndustryExpertise(){
-    return(
-        <div className="mx-auto mt-10 grid max-w-2l grid-cols-2 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-4">
-            {industries.map((industry) => (
-            <div key={industry.title} className="group flex flex-col mb-3">
-                <div>
-                    
-                </div>
-            </div>
-          ))}
-        </div>
+import Image from "next/image";
 
-    )
+const industries = [
+  {
+    title: "Logistics",
+    image: "/images/industries_logistics.jpg",
+    alt: "logistics",
+  },
+  { title: "Health", image: "/images/industries_health.jpg", alt: "Health" },
+  { title: "Finance", image: "/images/industries_finance.jpg", alt: "Finance" },
+  {
+    title: "Communication",
+    image: "/images/industries_communication.jpg",
+    alt: "Communication",
+  },
+];
+export function IndustryExpertise() {
+  return (
+    <div className="mx-auto mt-10 grid max-w-2l grid-cols-2 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-4">
+      {industries.map((industry) => (
+        <div
+          key={industry.title}
+          className="flex flex-col justify-center items-center h-48 max-h-96 border-2 border-gray-300"
+        >
+          <div className="w-2/3 h-2/3 flex object-center place-items-center ">
+            <Image
+              src={industry.image}
+              alt={industry.alt}
+              width={500}
+              height={500}
+            ></Image>
+          </div>
+          <h6 className="font-semibold">{industry.title}</h6>
+        </div>
+      ))}
+    </div>
+  );
 }
--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,18 +1,26 @@
 import Image from 'next/image'
 import ServicesSection from './components/ServicesSection'
+import { IndustryExpertise } from './components/IndustryExpertise'
 
 
 export default function ServicesPage() {
   return (
-    <div className="bg-white py-24 sm:py-32">
-      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+    <div className="bg-white py-12 sm:py-24">
+      <div className="mx-auto max-w-7xl px-6 py-12 lg:px-8">
         <div className="mx-auto max-w-2xl lg:mx-0">
           <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">ALL SERVICES</h2>
         </div>
         <ServicesSection></ServicesSection>
       </div>
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
+      <div className="mx-auto max-w-2xl lg:mx-0">
+          <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">INDUSTRY EXPERTISE</h2>
+        </div>
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <IndustryExpertise></IndustryExpertise>
+      </div>
       </div>
     </div>
+    
   )
 }
