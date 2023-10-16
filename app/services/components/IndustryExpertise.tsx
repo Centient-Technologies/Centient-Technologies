24310c34dec90ae0d2d085b0f1b11b3ddb9d9bc1
emmanaakai <naa.kai.odametey@gmail.com>
2023-10-16 21:50:55 +0000

    industry images boxfit containers

--- app/services/components/IndustryExpertise.tsx
+++ app/services/components/IndustryExpertise.tsx
@@ -18,20 +18,14 @@ export function IndustryExpertise() {
   return (
     <div className="mx-auto mt-10 grid max-w-2l grid-cols-2 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-4">
       {industries.map((industry) => (
-        <div
-          key={industry.title}
-          className="flex flex-col justify-center items-center h-48 max-h-96 border-2 border-gray-300"
-        >
-          <div className="w-2/3 h-2/3 flex object-center place-items-center ">
-            <Image
-              src={industry.image}
-              alt={industry.alt}
-              width={500}
-              height={500}
-            ></Image>
-          </div>
-          <h6 className="font-semibold">{industry.title}</h6>
+        <div key={industry.title} className="group flex flex-col mb-3">
+        <div className="w-5/6 h-5/6">
+          <Image src={industry.image} alt={industry.alt} width={500} height={500}></Image>
         </div>
+      <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600 text-center">
+          {industry.title}
+      </h3>
+    </div>
       ))}
     </div>
   );
